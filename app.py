f6955e4a62260e69fb480773af8e9ab644110541
thisishardik <hardy.lko@gmail.com>
2022-04-03 04:24:17 +0530

    Updated core logic for vector similarity

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import nltk
 from nltk.tokenize import RegexpTokenizer
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
 import yake
+import logging
 
 from flask import Flask, request, jsonify
 # from flask_cors import CORS
@@ -18,37 +21,30 @@ app = Flask(__name__)
 # app.config['CORS_HEADERS'] = 'Content-Type'
 
 
-def cos_similarity(Y):
-    l1 = []
-    l2 = []
+def cos_sim_vectors(vec1, vec2):
+    vec1 = vec1.reshape(1, -1)
+    vec2 = vec2.reshape(1, -1)
+    return cosine_similarity(vec1, vec2)[0][0]
 
+
+def cos_similarity(category_info):
     max_sim_category = ""
+    sim = 0
     max_sim = 0
 
     categories_map = fetch_categories_map()
+    similarity_vector = []
 
     for key, value in categories_map.items():
-        X = value
-        X = {x for x in X}
-        Y = {y for y in Y}
-        rvector = X.union(Y)
-        for w in rvector:
-            if w in X:
-                l1.append(1)
-            else:
-                l1.append(0)
-            if w in Y:
-                l2.append(1)
-            else:
-                l2.append(0)
-        c = 0
+        text = [value, category_info]
+        vectorizer = CountVectorizer().fit_transform(text)
+        vectors = vectorizer.toarray()
 
-        for i in range(len(rvector)):
-            c += l1[i]*l2[i]
-        cosine = c / float((sum(l1)*sum(l2))**0.5)
+        sim = cos_sim_vectors(vectors[0], vectors[1]) * 100
+        similarity_vector.append(sim)
 
-        if cosine > max_sim:
-            max_sim = cosine
+        if sim > max_sim:
+            max_sim = sim
             max_sim_category = key
 
     return max_sim, max_sim_category
@@ -61,9 +57,10 @@ def classify():
     query = args.get("query")
 
     kw_sent, kw_lst = keyword_extraction_yake(query)
-    kw_sent = sentence_tokenizer(kw_sent)
+    kw_sent = sentence_tokenizer(kw_lst)
+    category_info = generate_kw_info_from_kw_snt(kw_sent)
 
-    max_sim, max_sim_category = cos_similarity(query)
+    max_sim, max_sim_category = cos_similarity(category_info)
 
     return {
         "category": f"{max_sim_category}",
